7fd5a03f12b0a2e507dacef2fe0e28e9bab3c523
Artyom_G_ <ginoyan.artyom@gmail.com>
2023-03-27 23:08:53 +0400

    add remove file

--- routes/file.routes.js
+++ routes/file.routes.js
@@ -1,5 +1,5 @@
 const { verifyToken } = require('../middlewares/verifyToken');
-const { upload, getListFiles, download } = require('../controllers/file.controller');
+const { upload, getListFiles, download, remove } = require('../controllers/file.controller');
 
 module.exports = (app) => {
 	app.use((req, res, next) => {
@@ -9,4 +9,5 @@ module.exports = (app) => {
 	app.post('/upload', upload);
 	app.get('/files', getListFiles);
 	app.get('/files/:name', download);
+	app.delete('/files/:name', remove);
 };
