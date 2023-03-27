a2c6ef4a81269fee3284292efc39ed377c225e93
Artyom_G_ <ginoyan.artyom@gmail.com>
2023-03-27 14:07:58 +0400

    modified .env and models index file for connect to db

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 // 	dialect: config.dialect,
 // });
 
-const sequelize = new Sequelize("railway", process.env.DB_USER, process,env.DB_PASSWORD, {
+const sequelize = new Sequelize("railway", process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "mysql",
     port: process.env.DB_PORT
